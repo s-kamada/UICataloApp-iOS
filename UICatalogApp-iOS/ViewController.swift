d09f37af34410cf0cf3c96e1160e0fd31ea6d0f0
鎌田峻輔 <s.kamada@caraquri.com>
2019-09-23 01:00:12 +0900

    Initial Commit

---
+++ UICatalogApp-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICatalogApp-iOS
+//
+//  Created by 鎌田峻輔 on 2019/09/23.
+//  Copyright © 2019 personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
