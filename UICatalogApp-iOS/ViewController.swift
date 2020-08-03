d5cb16f23c34f7da2e50e946e9414a9f6dfc8d5a
Shunsuke Kamada <mod.rab7219@gmail.com>
2020-08-03 16:39:44 +0900

    cellが表示できるようにした

--- UICatalogApp-iOS/ViewController.swift
+++ UICatalogApp-iOS/ViewController.swift
@@ -11,29 +11,39 @@ import UIKit
 class ViewController: UIViewController {
 
 	@IBOutlet weak var previewLabel: UILabel!
-    var tableView: UITableView?
+    @IBOutlet weak var tableView: UITableView!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		navigationController?.title = "UILabel"
+//		navigationController?.title = "UILabel"
         
         previewLabel.isAccessibilityElement = false
+        tableView.delegate = self
+        tableView.dataSource = self
 	}
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return 1
-        }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
 
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            // TODO: なぜかcellのクラスを拾ってくれない
-            // Use of undeclared type 'PropertiesTableViewCell'
-            let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellReusableIdentifier, for: indexPath) as! PropertiesTableViewCell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellReusableIdentifier, for: indexPath) as! PropertiesTableViewCell
+        cell.detailLabel.text = "DETAIL"
+        cell.titleLabel.text = "TITLE"
+        cell.statusLabel.text = "STATUS"
+        cell.statusLabel.lineBreakMode = .byClipping
 
-            return UITableViewCell.init()
-        }
+        cell.backgroundColor = .red
+
+        return cell
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 }
 
 struct Constants {
